9e93217d4b4c2a0e9400c1fbc4da2d51d09708aa
alesantos99 <alessandrasantosds@gmail.com>
2019-09-30 16:04:41 -0300

    second

--- ForestHero/Scenes/level1.lua
+++ ForestHero/Scenes/level1.lua
@@ -41,12 +41,12 @@ local  buttonShoot
 
 local perspective = require("perspective")
 
---local camera = perspective.createView()
+local camera = perspective.createView()
 
-local camera
+--local camera
+local bullet
 
-
-
+local arrow
 
 --Hero sprisheet
 
@@ -59,9 +59,7 @@ local sheetOptions =
 
 local moveHero=  graphics.newImageSheet( "Images/spritesmovendo.png", sheetOptions )
 
-local  heroX 
 
-local  heroY 
 
 local sequences_hero = {
     -- first sequence (consecutive frames)
@@ -156,27 +154,33 @@ local sequenceAtack ={
 
 function scene:create( event )
 
-
-
 	local sceneGroup = self.view
-	camera = display.newGroup()
-	--physics.setDrawMode("debug")
+	
+	--physics.setDrawMode("hybrid")
 	physics.start()
 
 	physics.pause()
 
 
-	local background = display.newImageRect( "Images/bg1.png", screenW, screenH)
+	local background = display.newImageRect( "Images/bg.png", screenW, screenH)
 	background.x = display.screenOriginX
 	background.y = display.screenOriginY
 
 	background.anchorX = 0 
 	background.anchorY = 0
+
+	local background1 = display.newImageRect( "Images/bg0.png", screenW, screenH)
+	background1.x = screenW-50
+	background1.y = display.screenOriginY
+
+	background1.anchorX = 0 
+	background1.anchorY = 0
 	--background:setFillColor( .5 )
-	--background.alpha = 0.5
+	--background.alpha = 0.7
+	--background1.alpha = 0.7
 
 
-	local ground = display.newImageRect("Images/ground.png", screenW+2080,200)
+	local ground = display.newImageRect("Images/ground.png", screenW*4,200)
 
 	ground.x = display.screenOriginX
 	
@@ -189,7 +193,7 @@ function scene:create( event )
 	physics.addBody( ground, "static",{ bounce=0.0, friction=0.3 } )
 	
 
-	local ground1 = display.newImageRect("Images/plataforma1.png", 1000,200)
+	local ground1 = display.newImageRect("Images/asset3.png", 500,400)
 
 	ground1.x = display.contentCenterX
 	
@@ -201,6 +205,53 @@ function scene:create( event )
 
 	physics.addBody( ground1, "static",{ bounce=0.0, friction=0.3 } )
 	
+	
+
+
+	local ground2 = display.newImageRect("Images/asset3.png",800,200)
+
+	ground2.x = 1500
+	
+	ground2.y = display.contentHeight-200
+
+	ground2.id = "ground"
+
+	ground2.objType = "ground"
+
+
+	physics.addBody( ground2, "static",{ bounce=0.0, friction=0.3 } )
+	
+
+	local ground3 = display.newImageRect("Images/asset3.png",1200,400)
+
+	ground3.x = 2490
+	
+	ground3.y = display.contentHeight-300
+
+	ground3.id = "ground"
+
+	ground3.objType = "ground"
+
+
+	physics.addBody( ground3, "static",{ bounce=0.0, friction=0.3 } )
+	
+
+	local ground4 = display.newImageRect("Images/asset3.png",600,200)
+
+	ground4.x = 3300
+	
+	ground4.y = display.contentHeight-200
+
+	ground4.id = "ground"
+
+	ground4.objType = "ground"
+
+
+	physics.addBody( ground4, "static",{ bounce=0.0, friction=0.3 } )
+	
+
+	
+
 
 
 
@@ -209,8 +260,7 @@ function scene:create( event )
 	hero.id = "hero"
 	hero.y = display.contentHeight-172
 
-	heroX = hero.x
-	heroY = hero.y
+	
 	hero.speed = 1
 	
     hero.sensorOverlaps = 0
@@ -222,76 +272,100 @@ function scene:create( event )
 	hero.isFixedRotation = true
 
 
-	buttonLeft = display.newImageRect("Images/btnleft.png", 100,100)
+	buttonLeft = display.newImageRect("Images/btnleft.png", 200,200)
 
 	buttonLeft.id = "left"
 
 	buttonLeft.x= 200
 
-	buttonLeft.y = display.contentHeight-100
+	buttonLeft.y = display.contentHeight-200
 	
+	buttonLeft.alpha = 0.6
+
 	buttonLeft:addEventListener("touch",doControls)
 
 	 
-	buttonShoot = display.newImageRect("Images/btnarrow.png", 100,100)
 	
-
 
 
 	-- Move to right
 
 
-	buttonRight = display.newImageRect("Images/btright.png", 100,100)
+	buttonRight = display.newImageRect("Images/btright.png", 200,200)
 
 	buttonRight.id = "right"
 
-	buttonRight.x = 400
+	buttonRight.x = 450
 
- 	buttonRight.y = display.contentHeight-80
+	buttonRight.alpha = 0.6
+
+ 	buttonRight.y = display.contentHeight-200
 	buttonRight:addEventListener("touch", doControls)
 
 
 	-- jumpRight
 
-	buttonJump = display.newImageRect("Images/btnjump.png", 100,100)
+	buttonJump = display.newImageRect("Images/btnjump.png", 200,200)
 	buttonJump.id = "jump"
 
-	buttonJump.x = 1700
+	buttonJump.x = 1650
 
- 	buttonJump.y = display.contentHeight-80
+	buttonJump.alpha = 0.6
+
+ 	buttonJump.y = display.contentHeight-200
 	buttonJump:addEventListener("touch", doControls)
 
 	-- shoot
+	buttonShoot = display.newImageRect("Images/btnarrow.png", 200,200)
+	
 
 	buttonShoot.x = 1900
 
-	buttonShoot.y = display.contentHeight-80
+	buttonShoot.y = display.contentHeight-200
 
-	buttonShoot:addEventListener("touch", doControls)
+	buttonShoot.alpha = 0.6
+	buttonShoot:addEventListener("touch", shootArrow)
 
 	buttonShoot.id = "shoot"
 
 
 
 	Runtime:addEventListener("collision",jumpCollide)	
-	--camera:add(hero,1,true)	
+	camera:add(hero,1,true)	
 	
-	--camera:add(ground,2,false)	
-	
-	--camera:add(background,3,false)	
-	
-	--camera:setFocus(hero)
-	--camera:setBounds(0,display.contentWidth, 0, display.contentHeight)	
-	camera:insert(background)
+	camera:add(ground,2,false)	
+	camera:add(ground1,3,false)
+	camera:add(ground2,4,false)
 
-	camera:insert( ground)
+	camera:add(ground3,5,false)
+	camera:add(ground4,6,false)
+			
+	camera:add(background,7,false)	
+	camera:add(background1,8,false)	
+
+	camera:layer(2).paralaxRatio= 0.5
+	local levelWidth = display.actualContentWidth*4
+	--camera:setFocus(hero)
+	print("acw", display.actualContentWidth/2)
+	camera:setBounds(display.actualContentWidth/2,levelWidth-display.actualContentWidth*2-1200, -1000, display.contentHeight-1200)	
+	
+	if(hero.x > 1260)then
+		camera:remove(ground1)
+	end	
+
+	--camera:insert(background)
+
+	--camera:insert( ground)
 		
-	camera:insert( hero)
+	--camera:insert( hero)
 	
 	--sceneGroup:insert( background )
-	--sceneGroup:insert( ground)
+	--sceneGroup:insert( ground) 
 	--sceneGroup:insert( hero)
+
 	sceneGroup:insert(camera)
+	
+
 	
 	sceneGroup:insert( buttonLeft )
 	sceneGroup:insert( buttonRight)
@@ -302,17 +376,19 @@ end
 
 
 
+local hX , hY
+
 function doControls(event)
 	local pressed = event.target
 
+	
 	if event.phase == "began" then
 
 		if pressed.id == "left" then
-			if(hero.x>80 ) then
+			if(hero.x > 170) then
 				display.currentStage:setFocus(buttonLeft)
 				
-				print("Hero x",hero.x)
-				print("Hero y",hero.y)
+				
 				local  x = hero.x + hero.speed
 
 				hero:applyLinearImpulse(-200,0, x, hero.y)
@@ -320,6 +396,8 @@ function doControls(event)
 	    		hero:play()
 				print("Hero x",hero.x)
 				print("Hero y",hero.y)
+				hX = -hero.x
+				hY = hero.y
 
 				--hero.velocity = -hero.speed
 			end
@@ -330,10 +408,11 @@ function doControls(event)
 			hero:applyLinearImpulse(200,0, hero.x, hero.y)
 			hero:setSequence( "walkRight" )  -- switch to "fastRun" sequence
     		hero:play()
-			print("Hero x",hero.x)
-			print("Hero y",hero.y)
-
+		
 			--hero.velocity = hero.speed
+			hX = hero.x
+			hY = hero.y
+
 		elseif pressed.id == "jump" and hero.sensorOverlaps > 0 then
 			
 			
@@ -346,27 +425,11 @@ function doControls(event)
 				hero:setSequence( "jumpRight" )  -- switch to "fastRun" sequence
 	    		hero:play()
 				--heroJumps = false
-				print("Hero x",hero.x)
-				print("Hero y",hero.y)
+				
 				--heroJumps = heroJumps+1
-			
-		elseif pressed.id == "shoot" then
-			local bullet = display.newCircle( 100, 100, 10 )
-
-			bullet.x = hero.x
-
-			bullet.y = hero.y
-
-
-			physics.addBody( bullet, "dynamic", { radius=10 } )
-			bullet.gravityScale = 0
-			 
-			-- Make the object a "bullet" type object
-			bullet.isBullet = true
-			 
-			bullet:setLinearVelocity( 800, 0 )
-
-		end
+				
+		
+			end
 
 	elseif event.phase == "ended" then 	
 		if pressed.id == "left" then
@@ -380,7 +443,7 @@ function doControls(event)
 		elseif pressed.id == "jump" then
 			
 			display.currentStage:setFocus(nil)
-			
+		
 			
 		
 		end
@@ -389,7 +452,72 @@ function doControls(event)
 	end 
 
 end
+function shootArrow(event)
+	
 
+
+	local  pressed = event.target
+
+	print(event.target.id)
+	if event.phase == "began" then
+
+		if pressed.id == "shoot" then
+				
+				display.currentStage:setFocus(buttonShoot)
+				
+				--local bullet = display.newCircle( 100, 100, 10 )
+
+				--bullet.x = hX
+
+				---bullet.y = hY
+
+
+				--physics.addBody( bullet, "dynamic", { radius=10 } )
+				--bullet.gravityScale = 0
+				 
+				-- Make the object a "bullet" type object
+				--bullet.isBullet = true
+				 
+				--bullet:setLinearVelocity( 800, 0 )
+				--arrow = display.newImageRect("arrow.png",100,100)
+
+				--arrow.x = hX
+
+				---arrow.y = 500
+
+				--physics.addBody( arrow, "dynamic")
+
+				--arrow.isBullet = true
+
+				--arrow.gravityScale = 0
+				---arrow:setLinearVelocity( 800, 0 ) 
+
+				local arrow = display.newImageRect("Images/arrow.png",100,100)
+			
+				physics.addBody( arrow, "dynamic")
+
+				arrow.gravityScale = 0
+
+				arrow.isBullet = true
+				arrow.x = hero.x
+				arrow.y = hero.y
+				--transition.to ( arrow, { time = 1000, x = hero.X, y =-100} )
+				arrow:setLinearVelocity( 800, 0 )
+				--physics.addBody(arrow, "static", {density = 1, friction = 0, bounce = 0})
+
+				--transition.to ( arrow, { time = 1000, x = hX, y =-100} )
+
+	    end
+	elseif event.phase == "ended" then
+
+			display.currentStage:setFocus(nil)
+		
+		
+
+	end
+
+
+end
 
 function jumpCollide( event)
 
@@ -398,10 +526,10 @@ function jumpCollide( event)
 
  	local object2 = event.object2
  	
- 	print(object1.id,object2.id)
+ 	
 
  	if ( object1.id == "ground" and object2.id == "hero" ) then
- 		print("passei")	
+ 		
         -- Foot sensor has entered (overlapped) a ground object
         if ( event.phase == "began" ) then
             object2.sensorOverlaps = object2.sensorOverlaps + 1
@@ -424,7 +552,7 @@ function scene:show( event )
 		-- 
 		-- INSERT code here to make the scene come alive
 		-- e.g. start timers, begin animation, play audio, etc.
-		--camera:track()
+		camera:track()
 		physics.start()
 	end
 end
@@ -441,6 +569,7 @@ function scene:hide( event )
 		--
 		-- INSERT code here to pause the scene
 		-- e.g. stop timers, stop animation, unload sounds, etc.)
+
 		physics.stop()
 	elseif phase == "did" then
 		-- Called when the scene is now off screen
